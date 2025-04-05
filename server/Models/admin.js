160e942bbafea2070243f6e695e5d3852dae313c
Tejasvibihari <tejasvibihari2000@gmail.com>
2025-04-05 17:23:14 +0530

    Created Admin Model

---
+++ server/Models/admin.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const adminSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ['superadmin', 'admin'],
+        default: 'admin',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+const Admin = mongoose.model('Admin', adminSchema);
+export default Admin;
