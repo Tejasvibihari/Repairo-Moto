ef8ee46096d0e88a6abd08adcadd4ad74324c8fa
Tejasvibihari <tejasvibihari2000@gmail.com>
2025-04-08 19:46:05 +0530

    Update Employee edit form

--- client/src/components/EditEmployeeForm.jsx
+++ client/src/components/EditEmployeeForm.jsx
@@ -7,78 +7,126 @@ export default function EditEmployeeForm() {
         <>
             <div className=''>
                 <Heading heading="Add Employee" />
-                <form className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-6 shadow-sm border border-gray-300 rounded p-4'>
-                    <div className=''>
-                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
-                            First Name
-                        </label>
-                        <input
-                            type='text'
-                            className='border-2 border-gray-300 rounded-md p-2 w-full'
-                            required
-                            placeholder='First Name'
-                        />
+                <form className='shadow-sm border border-gray-300 rounded p-4'>
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-6 '>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                First Name
+                            </label>
+                            <input
+                                type='text'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='First Name'
+                            />
+                        </div>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                Last Name
+                            </label>
+                            <input
+                                type='text'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='Last Name'
+                            />
+                        </div>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                Phone Number
+                            </label>
+                            <input
+                                type='email'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='0000000000'
+                            />
+                        </div>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                Email
+                            </label>
+                            <input
+                                type='email'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='Email'
+                            />
+                        </div>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                Role
+                            </label>
+                            <select className='border-2 border-gray-300 rounded-md p-2 w-full'>
+                                <option disabled value="" selected>Select One</option>
+                                <option value="admin">Operation Manager</option>
+                                <option value="employee">Mechanic</option>
+                                <option value="employee">Delivery</option>
+                                <option value="manager">Manager</option>
+                                <option value="hr">HR</option>
+                                <option value="accountant">Accountant</option>
+                            </select>
+                        </div>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                Profile Image
+                            </label>
+                            <input
+                                type='file'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='Choose File'
+                            />
+                        </div>
                     </div>
-                    <div className=''>
-                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
-                            Last Name
-                        </label>
-                        <input
-                            type='text'
-                            className='border-2 border-gray-300 rounded-md p-2 w-full'
-                            required
-                            placeholder='Last Name'
-                        />
-                    </div>
-                    <div className=''>
-                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
-                            Phone Number
-                        </label>
-                        <input
-                            type='email'
-                            className='border-2 border-gray-300 rounded-md p-2 w-full'
-                            required
-                            placeholder='0000000000'
-                        />
-                    </div>
-                    <div className=''>
-                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
-                            Email
-                        </label>
-                        <input
-                            type='email'
-                            className='border-2 border-gray-300 rounded-md p-2 w-full'
-                            required
-                            placeholder='Email'
-                        />
-                    </div>
-                    <div className=''>
-                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
-                            Role
-                        </label>
-                        <select className='border-2 border-gray-300 rounded-md p-2 w-full'>
-                            <option disabled value="" selected>Select One</option>
-                            <option value="admin">Operation Manager</option>
-                            <option value="employee">Mechanic</option>
-                            <option value="employee">Delivery</option>
-                            <option value="manager">Manager</option>
-                            <option value="hr">HR</option>
-                            <option value="accountant">Accountant</option>
-                        </select>
-                    </div>
-                    <div className=''>
-                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
-                            Profile Image
-                        </label>
-                        <input
-                            type='file'
-                            className='border-2 border-gray-300 rounded-md p-2 w-full'
-                            required
-                            placeholder='Email'
-                        />
+                    <div className='grid grid-cols-1 md:grid-cols-4 gap-4 mt-4'>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                Address
+                            </label>
+                            <input
+                                type='text'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='Address'
+                            />
+                        </div>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                City
+                            </label>
+                            <input
+                                type='text'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='City'
+                            />
+                        </div>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                State
+                            </label>
+                            <input
+                                type='text'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='State'
+                            />
+                        </div>
+                        <div className=''>
+                            <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                                Pincode
+                            </label>
+                            <input
+                                type='number'
+                                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                                required
+                                placeholder='000000'
+                            />
+                        </div>
                     </div>
                     <button className='bg-primary text-white font-semibold py-2 px-4 rounded-md mt-4 col-span-1 md:col-span-3 cursor-pointer'>
-                        Edit Employee
+                        Add Employee
                     </button>
                 </form>
             </div>
--- server/Controllers/employeeController.js
+++ server/Controllers/employeeController.js
@@ -1,1 +1,36 @@
 import Employee from "../Models/employeeModel.js";
+
+export const createEmployee = async (req, res) => {
+    const { firstName, lastName, email, phone, role, address, city, state, pincode, referralCode } = req.body;
+    try {
+        const employee = await Employee.findOne({ email });
+        if (employee) {
+            return res.status(400).json({ message: "Employee already exists" });
+        }
+
+        const firstNameDigits = firstName.slice(0, 4);
+        const lastPhoneDigits = phone.slice(-4);
+        const password = `${firstNameDigits}${lastPhoneDigits}`;
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newEmployee = new Employee({
+            firstName,
+            lastName,
+            email,
+            phone,
+            role,
+            password: hashedPassword,
+            address,
+            city,
+            state,
+            pincode,
+            referralCode,
+            profilePicture: "https://example.com/default-profile-pic.jpg", // Placeholder URL
+        });
+        await newEmployee.save();
+        res.status(201).json({ message: "Employee created successfully" });
+    } catch (error) {
+        console.log(error)
+
+    }
+}
