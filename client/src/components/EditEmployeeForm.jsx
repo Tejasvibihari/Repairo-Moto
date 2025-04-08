ac968537785cd283dc0bb6fe9ae63b347988a0bc
Tejasvibihari <tejasvibihari2000@gmail.com>
2025-04-08 15:58:19 +0530

    Employee Ui Created

---
+++ client/src/components/EditEmployeeForm.jsx
@@ -0,0 +1,87 @@
+import React from 'react'
+import Heading from './ui/Heading'
+import TextField from '@mui/material/TextField';
+
+export default function EditEmployeeForm() {
+    return (
+        <>
+            <div className=''>
+                <Heading heading="Add Employee" />
+                <form className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-6 shadow-sm border border-gray-300 rounded p-4'>
+                    <div className=''>
+                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                            First Name
+                        </label>
+                        <input
+                            type='text'
+                            className='border-2 border-gray-300 rounded-md p-2 w-full'
+                            required
+                            placeholder='First Name'
+                        />
+                    </div>
+                    <div className=''>
+                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                            Last Name
+                        </label>
+                        <input
+                            type='text'
+                            className='border-2 border-gray-300 rounded-md p-2 w-full'
+                            required
+                            placeholder='Last Name'
+                        />
+                    </div>
+                    <div className=''>
+                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                            Phone Number
+                        </label>
+                        <input
+                            type='email'
+                            className='border-2 border-gray-300 rounded-md p-2 w-full'
+                            required
+                            placeholder='0000000000'
+                        />
+                    </div>
+                    <div className=''>
+                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                            Email
+                        </label>
+                        <input
+                            type='email'
+                            className='border-2 border-gray-300 rounded-md p-2 w-full'
+                            required
+                            placeholder='Email'
+                        />
+                    </div>
+                    <div className=''>
+                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                            Role
+                        </label>
+                        <select className='border-2 border-gray-300 rounded-md p-2 w-full'>
+                            <option disabled value="" selected>Select One</option>
+                            <option value="admin">Operation Manager</option>
+                            <option value="employee">Mechanic</option>
+                            <option value="employee">Delivery</option>
+                            <option value="manager">Manager</option>
+                            <option value="hr">HR</option>
+                            <option value="accountant">Accountant</option>
+                        </select>
+                    </div>
+                    <div className=''>
+                        <label className='text-gray-700 font-semibold mb-2 block text-sm'>
+                            Profile Image
+                        </label>
+                        <input
+                            type='file'
+                            className='border-2 border-gray-300 rounded-md p-2 w-full'
+                            required
+                            placeholder='Email'
+                        />
+                    </div>
+                    <button className='bg-primary text-white font-semibold py-2 px-4 rounded-md mt-4 col-span-1 md:col-span-3 cursor-pointer'>
+                        Edit Employee
+                    </button>
+                </form>
+            </div>
+        </>
+    )
+}
--- client/src/components/EmployeeCard.jsx
+++ client/src/components/EmployeeCard.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
-import { Trash, UserPen } from 'lucide-react'
+import { Check, CircleX, Trash, UserPen, X } from 'lucide-react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Heading from './ui/Heading';
+import AddEmployeeForm from './AddEmployeeForm';
+import EditEmployeeForm from './EditEmployeeForm';
 
 export default function EmployeeCard() {
+    const [open, setOpen] = React.useState(false);
+    const [openDelete, setOpenDelete] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleDeleteOpen = () => {
+        setOpenDelete(true);
+    };
+
+    const handleDeleteClose = () => {
+        setOpenDelete(false);
+    };
+
     return (
         <>
             <div className='border border-primary shadow-sm rounded-md p-4 mt-6 w-full'>
@@ -17,16 +45,66 @@ export default function EmployeeCard() {
                         <span className='text-sm font-inter'>Rating</span>
                     </div>
                     <div className='flex gap-2'>
-                        <button className='flex items-center justify-center bg-primary text-white py-2 rounded-md px-8 cursor-pointer hover:bg-transparent hover:text-primary border border-primary'>
+                        <button onClick={handleClickOpen} className='flex items-center justify-center bg-primary text-white py-2 rounded-md px-8 cursor-pointer hover:bg-transparent hover:text-primary border border-primary'>
                             <UserPen size={18} className='mr-2' /> Edit
                         </button>
-                        <button className='flex items-center justify-center bg-transparent text-red-600 py-2 rounded-md px-8 cursor-pointer hover:bg-red-600 hover:text-white border border-primary'>
+                        <button onClick={handleDeleteOpen} className='flex items-center justify-center bg-transparent text-red-600 py-2 rounded-md px-8 cursor-pointer hover:bg-red-600 hover:text-white border border-primary'>
                             <Trash size={18} className='mr-2' />    Delete
                         </button>
                     </div>
                 </div>
-
             </div>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                slotProps={{
+                    paper: {
+                        component: 'form',
+                        onSubmit: (event) => {
+                            event.preventDefault();
+                            const formData = new FormData(event.currentTarget);
+                            const formJson = Object.fromEntries(formData.entries());
+                            const email = formJson.email;
+                            console.log(email);
+                            handleClose();
+                        },
+                    },
+                }}
+            >
+                <DialogContent>
+                    <EditEmployeeForm />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                </DialogActions>
+            </Dialog>
+            <Dialog
+                open={openDelete}
+                onClose={handleDeleteClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    <Heading heading={"Delete Employee"} />
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Are you Sure Want to Delete Employee
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <button className='flex flex-row cursor-pointer hover:bg-primary hover:text-white border border-primary px-4 py-2 rounded text-primary'
+                        onClick={handleDeleteClose}>
+                        <X className='mr-2' />No</button>
+                    <button
+                        onClick={handleDeleteClose}
+                        autoFocus
+                        className='flex flex-row cursor-pointer bg-red-600 text-white border hover:border-red-600 px-4 py-2 rounded hover:text-red-600 hover:bg-transparent'
+                    >
+                        <Check className='mr-2' /> Yes
+                    </button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
