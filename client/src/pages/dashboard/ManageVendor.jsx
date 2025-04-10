8fd124367be4002b68509bf125d3422551f38d09
Tejasvibihari <tejasvibihari2000@gmail.com>
2025-04-10 15:06:32 +0530

    Search Input Ui for Vendor

--- client/src/pages/dashboard/ManageVendor.jsx
+++ client/src/pages/dashboard/ManageVendor.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Heading from '../../components/ui/Heading';
 import AddVendorForm from '../../components/AddVendorForm';
 import VendorProfileCard from '../../components/vendorProfileCard';
+import { Search } from 'lucide-react';
 
 export default function ManageVendor() {
     return (
@@ -13,6 +14,14 @@ export default function ManageVendor() {
                 </div>
                 <div className="bg-gray-100 p-4 rounded shadow">
                     <Heading heading={"All Vendors"} />
+                    <div className='relative'>
+                        <Search className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500' />
+                        <input
+                            type='text'
+                            placeholder='Search Vendor By Name'
+                            className='p-2 pl-10 border-gray-300 w-full my-4 rounded border'
+                        />
+                    </div>
                     <VendorProfileCard />
                 </div>
             </div>
